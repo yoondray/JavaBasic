abba846ea4c72d992889c3891ea14b73dc55503a
Yoo Park <74237804+yoondray@users.noreply.github.com>
2025-06-12 16:18:40 +0900

    Update While_1.java

--- src/main/java/While_1.java
+++ src/main/java/While_1.java
@@ -2,112 +2,33 @@ package src.main.java;
 
 public class While_1 {
 
+    //변수의 값을 복사해서 전달함 => Java는 num2의 값을 바꾸더라도 num1의 값은 바뀌지 않는다.
     public static void main(String[] args) {
+        int num1 = 5;
+        System.out.println("1. changeNum 호출 전 = " + num1);
+        changeNum(num1);
+        System.out.println("4. changeNum 호출 후 = " + num1);
 
-        //반복문
-        int count = 0;
+        changeNum(num1);
+        //changeNum(5);
 
-        count = count + 1;
-        System.out.println("현재 숫자는 = " + count);
+        changeInt(num1);
+        System.out.println("5. num1에 값 복사 전 = " + num1);
+        num1 = changeInt(num1);
+        System.out.println("6. changeInt 호출 후 = " + num1);
 
-        count = count + 1;
-        System.out.println("현재 숫자는 = " + count);
+    }
 
-        count = count + 1;
-        System.out.println("현재 숫자는 = " + count);
-        //결과) 현재 숫자는 = 3
+    public static void changeNum(int num2) {
+        //int num2 = 5;
+        System.out.println("2. num2값 변경 전 = " + num2);
+        num2 = num2 * 2;
+        System.out.println("3. num2값 변경 후 = " + num2);
+        num2 = 10;
+    }
 
-        while (count < 3) {
-            count = count + 1;      //count ++;
-            System.out.println("현재 숫자는 = " + count);
-        }
-
-        // false이면 빠져나갈 수 있다.
-        /* while (false) {
-
-         }*/
-
-        //변경에 유연하지 않다.
-        int sum = 0;
-
-        sum = sum + 10;
-        System.out.println("i = "+ 10 + "sum = " + sum); //10
-        sum = sum + 11;
-        System.out.println("i = "+ 11 + "sum = " + sum); //21
-        sum = sum + 12;
-        System.out.println("i = "+ 12 + "sum = " + sum); //33
-
-
-        //변수(i)를 사용해서 더 변경하기 쉬운 코드로 만들자.
-        int i= 10;
-
-        sum = sum + i;
-        i++;
-        System.out.println("i = "+ i + "sum = " + sum); //10
-
-        sum = sum + i;
-        i++;
-        System.out.println("i = "+ i + "sum = " + sum); //21
-
-        sum = sum + i;
-        i++;
-        System.out.println("i = "+ i + "sum = " + sum); //33
-
-
-        int j = 1;
-        int endNum =3;
-
-        while (j <= endNum) {
-            sum = sum + j;
-            System.out.println("j = "+ j + "sum = " + sum);
-            j++;
-        }
-        //결과)j=1 sum=1 / j=2 sum=3 / j=3 sum=6
-
-
-        //do-while 최초 한번은 항상 실행된다. 조건과 상관없이 무조건 while 출력
-        do {
-            System.out.println("현재숫자는 = " +j);
-            j++;
-
-        } while (j > endNum);
-
-
-        //break - 반복문 즉시종료
-        //continue - 즉시 다음 조건문 실행
-        int total = 0;
-
-        //계속실행 (무제한)
-        while(true) {
-            total = total + j;  //+1+2+3...
-            if(total > 10) {
-                System.out.println("합이 10보다 크면 종료 : j= " + "total= " + total);
-                break;
-            }
-            j++;
-        }
-
-        while (j <= 5) {
-            if(j == 3) {
-                j++;
-                continue;
-            }
-            //j가 3일때 true이기때문에 실행안됨
-            System.out.println(j);
-            j++;
-
-        }
-
-        sum = sum + j;
-        j++;
-        System.out.println("j = "+ j + "sum = " + sum); //10
-
-        sum = sum + j;
-        j++;
-        System.out.println("j = "+ j + "sum = " + sum); //21
-
-        sum = sum + j;
-        j++;
-        System.out.println("j = "+ j + "sum = " + sum); //33
+    public static int changeInt(int number) {
+        number = number * 2;
+        return number;
     }
 }
